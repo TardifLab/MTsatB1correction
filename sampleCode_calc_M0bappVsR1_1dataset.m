09ca6335d98a4e28c31bc7f803666b7e445d962c
Christopher Rowley <chris_rowley18@hotmail.com>
2021-08-04 11:39:24 -0400

    Adjust for dual TR's used in VFA

--- sampleCode_calc_M0bappVsR1_1dataset.m
+++ sampleCode_calc_M0bappVsR1_1dataset.m
@@ -85,16 +85,22 @@ figure; imshow3Dfullseg(lfa, [150 600], mask)
 %% Calculate A0 and R1
 low_flip_angle = 5;    % flip angle in degrees -> USER DEFINED
 high_flip_angle = 20;  % flip angle in degrees -> USER DEFINED
-TR = 30;               % repetition time of the GRE kernel in milliseconds -> USER DEFINED
+TR1 = 30;               % low flip angle repetition time of the GRE kernel in milliseconds -> USER DEFINED
+TR2 = 30;               % high flip angle repetition time of the GRE kernel in milliseconds -> USER DEFINED
 
 a1 = low_flip_angle*pi/180 .* b1; % note the inclusion of b1 here.
 a2 = high_flip_angle*pi/180 .* b1; 
 
-R1 = 0.5 .* (hfa.*a2./ TR - lfa.*a1./TR) ./ (lfa./(a1) - hfa./(a2));
+% New code Aug 4, 2021 CR for two TR's
+R1 = 0.5 .* (hfa.*a2./ TR2 - lfa.*a1./TR1) ./ (lfa./(a1) - hfa./(a2));
+App = lfa .* hfa .* (TR1 .* a2./a1 - TR2.* a1./a2) ./ (hfa.* TR1 .*a2 - lfa.* TR2 .*a1);
+
+% Old code for single TR only
+%R1 = 0.5 .* (hfa.*a2./ TR - lfa.*a1./TR) ./ (lfa./(a1) - hfa./(a2));
+% App = lfa .* hfa .* (TR .* a2./a1 - TR.* a1./a2) ./ (hfa.* TR .*a2 - lfa.* TR .*a1);
+
 R1 = R1.*mask;
 T1 = 1/R1  .* mask;
-
-App = lfa .* hfa .* (TR .* a2./a1 - TR.* a1./a2) ./ (hfa.* TR .*a2 - lfa.* TR .*a1);
 App = App .* mask;
 
 %check them
